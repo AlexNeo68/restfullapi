1a7fbeec9631c71aa89fe6a3fe92b74bc719cce3
alexneo68 <tunning_brain@mail.ru>
2019-06-04 18:17:34 +0300

    add complex buyer operations

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
+/* Buyers controllers */
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['show', 'index']]);
+Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
+Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
+Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['index']]);
+Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' => ['index']]);
+
+
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['show', 'index']]);
 Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
 Route::resource('products', 'Product\ProductController', ['only' => ['show', 'index']]);
+
 Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['show', 'index']]);
 Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' => ['index']]);
 Route::resource('transactions.sellers', 'Transaction\TransactionSellerController', ['only' => ['index']]);
+
 Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
