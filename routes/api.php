1e297c58b4dcd8fedddb46c4892ff2133ab0472d
AlexNeo68 <tunning_brain@mail.ru>
2019-06-05 22:50:54 +0300

    add complex operations for product

--- routes/api.php
+++ routes/api.php
@@ -41,6 +41,7 @@ Route::resource('products.transactions', 'Product\ProductTransactionController',
 Route::resource('products.buyers', 'Product\ProductBuyerController', ['only' => ['index']]);
 Route::resource('products.categories', 'Product\ProductCategoryController', ['only' => ['index', 'update', 'destroy']]);
 Route::resource('products.buyers.transactions', 'Product\ProductBuyerTransactionController', ['only' => ['store']]);
+Route::resource('products.sellers', 'Product\ProductSellerController', ['only' => ['index']]);
 
 
 
